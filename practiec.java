ca2c64dacbf488e773f903d64ac0e3eb7ac94405
Swaraj Bandhu Prasad Kalwar <swarajbandhi0000@gmail.com>
2024-11-27 11:51:29 +0530

    done with alert handling in practice java file

--- practiec.java
+++ practiec.java
@@ -1,5 +1,6 @@
 package exampractice;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.edge.EdgeDriver;
 
@@ -12,6 +13,16 @@ public class practiec {
 		driver.findElement(By.xpath("/html/body/form/input[1]")).sendKeys("swarajbandhi0000@gmail.com");
 		driver.findElement(By.xpath("/html/body/form/input[2]")).sendKeys("swaraj1234");
 		driver.findElement(By.xpath("/html/body/form/input[3]")).submit();
+//		String title = driver.getTitle();
+//		System.out.println("The title of the link is:" + title);
+//		String url = driver.getCurrentUrl();
+//		System.out.println("The link is:" + url);
+//		
+	    String text1 = driver.switchTo().alert().getText();
+		System.out.println("The alert message is:" + text1);
+		driver.switchTo().alert().accept();
+//		System.out.println("The link is " + driver.getCurrentUrl());
+//	    System.out.println("The link is working and script has passed");
 
 	}
 
